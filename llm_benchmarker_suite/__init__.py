ba37a329b7ead0930b3426c052c5f6e1dd07b49a
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-07-28 17:54:32 -0400

    bug fix

--- llm_benchmarker_suite/__init__.py
+++ llm_benchmarker_suite/__init__.py
@@ -1,2 +1,5 @@
 # llm_benchmarking_suite/__init__.py
-import opencompass.opencompass as opencompass
+from .opencompass import opencompass as opencompass
+
+# Specify which symbols to export from opencompass
+__all__ = dir(opencompass)
